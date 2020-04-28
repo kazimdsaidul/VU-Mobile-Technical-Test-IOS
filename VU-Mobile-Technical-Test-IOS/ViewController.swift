b67ab2391560c85f6049f1ab26b583e1f71b41e0
Kazi Md. Saidul <kazimdsaidul@gmail.com>
2020-04-28 09:53:04 +0600

    Initial Commit

---
+++ VU-Mobile-Technical-Test-IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VU-Mobile-Technical-Test-IOS
+//
+//  Created by Kazi Md. Saidul on 4/28/20.
+//  Copyright © 2020 Kazi Md. Saidul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
